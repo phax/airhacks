92c5195d05a54e5ed5043fedaca0a624672d92b9
Adam Bien <abien@adam-bien.com>
2024-12-01 16:39:40 +0100

    jlama sentiment analysis example imported

---
+++ jlama-sentiment/src/main/java/airhacks/sentimental/control/ModelLoader.java
@@ -0,0 +1,36 @@
+package airhacks.sentimental.control;
+
+import java.io.IOException;
+
+import com.github.tjake.jlama.model.AbstractModel;
+import com.github.tjake.jlama.model.ModelSupport;
+import com.github.tjake.jlama.safetensors.DType;
+import com.github.tjake.jlama.safetensors.SafeTensorSupport;
+
+public interface ModelLoader {
+    enum Model{
+        TINY("tjake/TinyLlama-1.1B-Chat-v1.0-Jlama-Q4"),
+        LLAMA("tjake/Llama-3.2-1B-Instruct-Jlama-Q4");
+        
+        final String modelName;
+        
+        private Model(String modelName) {
+            this.modelName = modelName;
+        }
+
+        public String modelName(){
+            return this.modelName;
+        }
+
+
+    }
+        String workingDirectory = "./models";
+
+
+    static AbstractModel load() throws IOException{
+        var localModelPath = SafeTensorSupport
+        .maybeDownloadModel(workingDirectory, Model.LLAMA.modelName());
+        return ModelSupport.loadModel(localModelPath, DType.F32, DType.I8);
+    }
+
+}
